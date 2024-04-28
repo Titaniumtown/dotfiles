2de85b03de2bcb545807b463bd932c8f5ce24e68
Simon Gardling <titaniumtown@proton.me>
2024-04-28 17:36:37 -0400

    adwaita icon themeE

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -238,6 +238,10 @@
       package = pkgs.adw-gtk3;
       name = "adw-gtk3-dark";
     };
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Breeze";
+    };
   };
 
   qt = {
